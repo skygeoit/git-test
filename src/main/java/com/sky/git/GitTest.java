59e1bcb1020f369bb6f311cac48fcc5e57bea517
sky <zhcmaow@163.com>
2021-10-31 19:57:54 +0800

    commit gitlab

--- src/main/java/com/sky/git/GitTest.java
+++ src/main/java/com/sky/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hot!");
         System.out.println("push!");
         System.out.println("pull!");
+        System.out.println("gitlab");
 
     }
 }
