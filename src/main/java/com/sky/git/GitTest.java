572efa9b582415bdcd5445fda50d4723a4f71924
skygeoit <93460446+skygeoit@users.noreply.github.com>
2021-10-31 15:50:41 +0800

    pull

--- src/main/java/com/sky/git/GitTest.java
+++ src/main/java/com/sky/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("master!");
         System.out.println("hot!");
         System.out.println("push!");
+        System.out.println("pull!");
 
     }
 }
