1bf48e084e15adc1a941c2c5a7689c7f70540b1d
sky <zhcmaow@163.com>
2021-10-31 15:09:48 +0800

    third commit

--- src/main/java/com/sky/git/GitTest.java
+++ src/main/java/com/sky/git/GitTest.java
@@ -3,7 +3,8 @@ package com.sky.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hellp git!");
-        System.out.println("hellp git!");
+        System.out.println("hellp git1!");
+        System.out.println("hello git!");
 
 
     }
